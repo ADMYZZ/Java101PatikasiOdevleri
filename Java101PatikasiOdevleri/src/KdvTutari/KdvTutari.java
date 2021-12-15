2132c1c7b44eaf050ebe1552e95e894bf3865240
A.yaz <adem.yazzz@gmail.com>
2021-12-15 21:52:51 +0300

    KDV tutarı hesaplama ödevi yapıldı.

---
+++ Java101PatikasiOdevleri/src/KdvTutari/KdvTutari.java
@@ -0,0 +1,20 @@
+package KdvTutari;
+import java.util.Scanner;
+
+public class KdvTutari {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double kdvOrani,kdvTutari,tutar,toplamTutar;
+
+        System.out.print("Fiyatı giriniz : ");
+        tutar = input.nextDouble();
+
+        kdvOrani = (tutar <= 1000) ? 0.08 : 0.18;
+        kdvTutari = tutar * kdvOrani;
+        toplamTutar = tutar + kdvTutari;
+
+        System.out.println("KDV'siz Tutar : " + tutar);
+        System.out.println("KDV Tutarı : " + kdvTutari);
+        System.out.println("KDV'li Toplam Tutar : " + toplamTutar);
+    }
+}
