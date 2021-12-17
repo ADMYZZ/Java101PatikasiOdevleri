a46e4a0314de5051155b3c3918012aa8e240de16
A.yaz <adem.yazzz@gmail.com>
2021-12-17 16:44:48 +0300

    Dairenin çevresini ve alanını hesaplama ödevi yapıldı.

---
+++ Java101PatikasiOdevleri/src/DaireninAlaniVeCevresi/DaireninAlaniCevresi.java
@@ -0,0 +1,23 @@
+package DaireninAlaniVeCevresi;
+import java.util.Scanner;
+
+public class DaireninAlaniCevresi {
+    public static void main(String[] args) {
+        int r;
+        double alan, alfa, cevre, pi =3.14;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Dairenii yarıçapını giriniz : ");
+        r = input.nextInt();
+
+        System.out.print("Kaç derecelik alanı tarayalım(0-360 arasında değer giriniz.) : ");
+        alfa = input.nextInt();
+
+        alan = (pi * r * r) * (alfa / 360);
+        cevre = (2 * pi *r) * (alfa / 360);
+        System.out.println(alfa + "Derecelik dairenin alanı : " + alan +"\n"
+                           + alfa + "Derecelik dairenin çevresi : " + cevre);
+
+
+    }
+}
