00e25ac7b7c605840934839e38d8496cb115679d
A.yaz <adem.yazzz@gmail.com>
2022-01-03 19:52:24 +0300

    Girilen sayılardan en büyük ve en küçük olanı ekrana yazdıran program.

---
+++ Java101PatikasiOdevleri/src/Donguler/MaxMinNumber/MaxMinNumber.java
@@ -0,0 +1,35 @@
+package Donguler.MaxMinNumber;
+import java.util.Scanner;
+
+public class MaxMinNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int counter = 1, input, maxNumber = 0, minNumber = 0, number;
+
+        while (true){
+            System.out.print("Kaç tane sayı gireceksiniz : ");
+            input = scan.nextInt();
+            System.out.println("------------------------------------");
+
+            if (input > 0){
+                break;
+            }else {
+                System.out.print("Lütfen pozitif bir sayı giriniz. ");
+            }
+        }
+
+        while (counter <= input){
+            System.out.print(counter + ". sayıyı giriniz : ");
+            number = scan.nextInt();
+            if (number < minNumber){
+                minNumber = number;
+            }else if (maxNumber < number){
+                maxNumber = number;
+            }
+            counter++;
+        }
+        System.out.println("------------------------------------");
+        System.out.println("Girgiğniz sayılardan en büyük olan : " + maxNumber);
+        System.out.println("Girgiğniz sayılardan en küçük olan : " + minNumber);
+    }
+}
