626e4007f0fddeabec5ea0d9e047593f076b315a
A.yaz <adem.yazzz@gmail.com>
2022-01-21 14:58:52 +0300

    Dizinin harmonik ortalamasını hesaplayan program.

---
+++ Java101PatikasiOdevleri/src/Array/sumOfArray.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class sumOfArray {
+    public static void main(String[] args) {
+        double sum = 0.0;
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int number : numbers) {
+            sum += 1.0 / number;
+        }
+        System.out.println(sum);
+    }
+}
